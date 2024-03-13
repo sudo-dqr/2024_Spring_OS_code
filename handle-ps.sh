ae01b4efdef1052cb2fda9d6b7019a79e7d7b56a
杜启嵘 <22373362@buaa.edu.cn>
2024-03-13 21:24:57 +0800

    2

--- handle-ps.sh
+++ handle-ps.sh
@@ -37,7 +37,7 @@ if $SORT; then
     sort -k4nr -k2n $FILE
 elif [ ! -z "$CMD" ]; then
     # Your code here. (2/3)
-    awk '$5~"$CMD"{print $0}' $FILE
+    awk -v input=$CMD '$5~input{print $0}' $FILE
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
     awk '{print $0}' $FILE
