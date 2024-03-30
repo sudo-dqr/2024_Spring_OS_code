67d6d17210faf6fff40919f9e42e537b39b77e43
杜启嵘 <22373362@buaa.edu.cn>
2024-03-30 22:39:21 +0800

    the first half

--- include/queue.h
+++ include/queue.h
@@ -116,9 +116,8 @@
 	/* Exercise 2.2: Your code here. */  \
 	do {
 		LIST_NEXT((elm), field) = LIST_NEXT((listelm), field);    
-		if (LIST_NEXT((listelm), field) != NULL) {                            
+		if (LIST_NEXT((listelm), field) != NULL)                        
 			LIST_NEXT((listelm), field)->field.le_prev = &LIST_NEXT((elm), field);
-		}
 		LIST_NEXT((listelm), field) = (elm);
 		(elm)->field.le_prev = &LIST_NEXT((listelm), field);
 	} while (0)
