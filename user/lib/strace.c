a4cea1a01071ac72cd192a006cffea2c41e9dd2c
OSome-Caesium <caesium@os.buaa.edu.cn>
2024-05-22 11:01:40 +0000

    init lab4-strace

---
+++ user/lib/strace.c
@@ -0,0 +1,24 @@
+#include <lib.h>
+
+void strace_barrier(u_int env_id) {
+	int straced_bak = straced;
+	straced = 0;
+	while (envs[ENVX(env_id)].env_status == ENV_RUNNABLE) {
+		syscall_yield();
+	}
+	straced = straced_bak;
+}
+
+void strace_send(int sysno) {
+	if (!((SYS_putchar <= sysno && sysno <= SYS_set_tlb_mod_entry) ||
+	      (SYS_exofork <= sysno && sysno <= SYS_panic)) ||
+	    sysno == SYS_set_trapframe) {
+		return;
+	}
+
+	// Your code here. (1/2)
+}
+
+void strace_recv() {
+	// Your code here. (2/2)
+}
