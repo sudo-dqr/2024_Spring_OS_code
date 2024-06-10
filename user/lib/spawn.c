471552dfa3a0dce25b59b7c7a44a2220d11f6e7b
杜启嵘 <22373362@buaa.edu.cn>
2024-06-10 21:56:10 +0800

    [add] : Exercise 6.4 6.5

--- user/lib/spawn.c
+++ user/lib/spawn.c
@@ -122,7 +122,12 @@ int spawn(char *prog, char **argv) {
 	int r;
 	u_char elfbuf[512];
 	/* Exercise 6.4: Your code here. (1/6) */
-
+	if ((r = readn(fd, elfbuf, sizeof(Elf32_Ehdr))) != sizeof(Elf32_Ehdr)) {
+		if (r >= 0) {
+			r = -E_NOT_EXEC;
+		}
+		goto err;
+	}
 	const Elf32_Ehdr *ehdr = elf_from(elfbuf, sizeof(Elf32_Ehdr));
 	if (!ehdr) {
 		r = -E_NOT_EXEC;
@@ -134,12 +139,17 @@ int spawn(char *prog, char **argv) {
 	// If the syscall fails, set 'r' and 'goto err'.
 	u_int child;
 	/* Exercise 6.4: Your code here. (2/6) */
-
+	if ((child = syscall_exofork()) < 0) {
+		r = child;
+		goto err;
+	}
 	// Step 4: Use 'init_stack(child, argv, &sp)' to initialize the stack of the child.
 	// 'goto err1' if that fails.
 	u_int sp;
 	/* Exercise 6.4: Your code here. (3/6) */
-
+	if ((r = init_stack(child, argv, &sp)) < 0) {
+		goto err1;
+	}
 	// Step 5: Load the ELF segments in the file into the child's memory.
 	// This is similar to 'load_icode()' in the kernel.
 	size_t ph_off;
@@ -149,7 +159,15 @@ int spawn(char *prog, char **argv) {
 		// 'goto err1' on failure.
 		// You may want to use 'seek' and 'readn'.
 		/* Exercise 6.4: Your code here. (4/6) */
-
+		if ((r = seek(fd, ph_off)) < 0) {
+			goto err1;
+		} 
+		if ((r = read(fd, elfbuf, ehdr->e_phentsize)) != ehdr->e_phentsize) {
+			if (r >= 0) {
+				r = -E_NOT_EXEC;
+				goto err1;
+			}
+		}
 		Elf32_Phdr *ph = (Elf32_Phdr *)elfbuf;
 		if (ph->p_type == PT_LOAD) {
 			void *bin;
@@ -157,12 +175,16 @@ int spawn(char *prog, char **argv) {
 			// using 'read_map()'.
 			// 'goto err1' if that fails.
 			/* Exercise 6.4: Your code here. (5/6) */
-
+			if ((r = read_map(fd, ph->p_offset, &bin)) < 0) {
+				goto err1;
+			}
 			// Load the segment 'ph' into the child's memory using 'elf_load_seg()'.
 			// Use 'spawn_mapper' as the callback, and '&child' as its data.
 			// 'goto err1' if that fails.
 			/* Exercise 6.4: Your code here. (6/6) */
-
+			if ((r = elf_load_seg(ph, bin, spawn_mapper, &child)) != 0) {
+				goto err1;
+			}
 		}
 	}
 	close(fd);
--- user/sh.c
+++ user/sh.c
@@ -91,9 +91,15 @@ int parsecmd(char **argv, int *rightpipe) {
 			// utilize 'debugf' to print relevant messages,
 			// and subsequently terminate the process using 'exit'.
 			/* Exercise 6.5: Your code here. (1/3) */
-
-			user_panic("< redirection not implemented");
-
+			if ((fd = open(t, O_RDONLY)) < 0) {
+				debugf("failed to open %s\n");
+				exit();
+			}
+			if ((r = dup(fd, 0)) < 0) {
+				debugf("failed to duplicate file to <stdin>\n");
+				exit();
+			}
+			close(fd);
 			break;
 		case '>':
 			if (gettoken(0, &t) != 'w') {
@@ -106,9 +112,15 @@ int parsecmd(char **argv, int *rightpipe) {
 			// utilize 'debugf' to print relevant messages,
 			// and subsequently terminate the process using 'exit'.
 			/* Exercise 6.5: Your code here. (2/3) */
-
-			user_panic("> redirection not implemented");
-
+			if ((fd = open(t, O_WRONLY)) < 0) {
+				debugf("failed to open %s\n");
+				exit();
+			}
+			if ((r = dup(fd, 1)) < 0) {
+				debugf("failed to duplicate file to <stdout>\n");
+				exit();
+			}
+			close(fd);
 			break;
 		case '|':;
 			/*
@@ -128,9 +140,21 @@ int parsecmd(char **argv, int *rightpipe) {
 			 */
 			int p[2];
 			/* Exercise 6.5: Your code here. (3/3) */
-
-			user_panic("| not implemented");
-
+			if ((r = pipe(p)) < 0) {
+				debugf("failed to create pipe\n");
+				exit();
+			}
+			if ((*rightpipe = fork()) == 0) {
+				dup(p[0], 0);
+				close(p[0]);
+				close(p[1]);
+				return parsecmd(argv, rightpipe);
+			} else {
+				dup(p[1], 1);
+				close(p[1]);
+				close(p[0]);
+				return argc;
+			}
 			break;
 		}
 	}
